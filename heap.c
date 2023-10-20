0cbfcef6c871076daa66c72f93c24ed934bc7fbc
agamabergel <agam123abergel@gmail.com>
2023-10-20 23:50:59 +0300

    added basics

---
+++ heap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define HEAP_CAP_SIZE 640000
+char heap[HEAP_CAP_SIZE];
+
+
+typedef struct 
+{
+	void* .start;
+	size_t .size;		
+} heap_chunk;
+
+
+void* mmalloc
+
+int main(void)
+{
+
+	return 0;
+}
+
